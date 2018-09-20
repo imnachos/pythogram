450ab73654903d6e4d0041957e0fd5d2e24f7816
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-09-20 20:02:18 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # pythogram
 
-Simple project to learn some stuff.
+Sample project to demonstrate how to use Intagram's python API.
+
+
+# Instructions
+
+-Install the requirements.
+
+-Create a file called 'settings.py' at the root folder of the project with the following variables:
+
+USER = "username"
+PASSWORD = "password"
+
+-Run main.py
---
+++ pythogram/utilities.py
@@ -0,0 +1,35 @@
+def getTotalFollowers(api, user_id):
+    """
+    Returns the list of followers of the user.
+    It should be equivalent of calling api.getTotalFollowers from InstagramAPI
+    """
+
+    followers = []
+    next_max_id = True
+    while next_max_id:
+        # first iteration hack
+        if next_max_id is True:
+            next_max_id = ''
+
+        _ = api.getUserFollowers(user_id, maxid=next_max_id)
+        followers.extend(api.LastJson.get('users', []))
+        next_max_id = api.LastJson.get('next_max_id', '')
+    return followers
+
+def getTotalFollowings(api, user_id):
+    """
+    Returns the list of followings of the user.
+    It should be equivalent of calling api.getTotalFollowers from InstagramAPI
+    """
+
+    followings = []
+    next_max_id = True
+    while next_max_id:
+        # first iteration hack
+        if next_max_id is True:
+            next_max_id = ''
+
+        _ = api.getUserFollowings(user_id, maxid=next_max_id)
+        followings.extend(api.LastJson.get('users', []))
+        next_max_id = api.LastJson.get('next_max_id', '')
+    return followings
