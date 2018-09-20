d91c7396acfead63ac17a6cfd08d2c24341016c9
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2018-09-20 20:09:47 -0300

    Cleaned up some bad naming.

--- pythogram/utilities.py
+++ pythogram/utilities.py
@@ -1,4 +1,5 @@
-def getTotalFollowers(api, user_id):
+
+def get_total_followers(api, user_id):
     """
     Returns the list of followers of the user.
     It should be equivalent of calling api.getTotalFollowers from InstagramAPI
@@ -7,29 +8,28 @@ def getTotalFollowers(api, user_id):
     followers = []
     next_max_id = True
     while next_max_id:
-        # first iteration hack
         if next_max_id is True:
             next_max_id = ''
 
-        _ = api.getUserFollowers(user_id, maxid=next_max_id)
+        response = api.getUserFollowers(user_id, maxid=next_max_id)
         followers.extend(api.LastJson.get('users', []))
         next_max_id = api.LastJson.get('next_max_id', '')
     return followers
 
-def getTotalFollowings(api, user_id):
+
+def get_total_followings(api, user_id):
     """
     Returns the list of followings of the user.
-    It should be equivalent of calling api.getTotalFollowers from InstagramAPI
+    It should be equivalent of calling api.getTotalFollowings from InstagramAPI
     """
 
     followings = []
     next_max_id = True
     while next_max_id:
-        # first iteration hack
         if next_max_id is True:
             next_max_id = ''
 
-        _ = api.getUserFollowings(user_id, maxid=next_max_id)
+        response = api.getUserFollowings(user_id, maxid=next_max_id)
         followings.extend(api.LastJson.get('users', []))
         next_max_id = api.LastJson.get('next_max_id', '')
     return followings
