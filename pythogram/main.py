1612434545f2cfaf63100c49baf83edc6c270273
Ignacio Fernandez Giacometti <fernandez.giacometti@gmail.com>
2019-01-13 22:36:12 -0300

    Added simple cookie manager as a proof of concept, not really working for now.
    Added dumping follower list to check update since last query.
    
    #commiting unfinished work for backup purposes

--- pythogram/main.py
+++ pythogram/main.py
@@ -3,8 +3,14 @@ from pythogram import settings as cfg, utilities as util
 
 api = InstagramAPI(cfg.USER, cfg.PASSWORD)
 
+
+saved_cookies = util.get_last_session_cookies()
+if saved_cookies is not None:
+    api.s.cookies = saved_cookies
+
 if api.login():
 
+    util.save_last_session_cookies(api.s)
     print("Login succes!")
 
     followers_json = util.get_total_followers(api, api.username_id)
@@ -24,12 +30,19 @@ if api.login():
     not_following_back = list(set(followings_list) - set(followers_list))
     not_following_them = list(set(followers_list) - set(followings_list))
 
+
+    last_followers_list = util.get_follower_list()
+    stopped_following_since_last = list(set(last_followers_list) - set(followers_list))
+    print('Users that stopped following since last time: ', stopped_following_since_last)
+    util.save_follower_list(followers_list)
+
     print('Users that are not following back: ', not_following_back)
     print('Users that you are not following: ', not_following_them)
 
 
 else:
     print("Can't login!")
+    util.clean_cookies()
 
 
 
--- pythogram/utilities.py
+++ pythogram/utilities.py
@@ -1,3 +1,40 @@
+import json, pickle
+
+
+def save_last_session_cookies(session):
+    with open('session', 'wb') as f:
+        pickle.dump(session.cookies, f)
+
+
+def save_follower_list(followers):
+    with open('followers.txt', 'wb') as f:
+        pickle.dump(followers, f)
+
+
+def get_follower_list():
+    with open('followers.txt', 'rb') as f:
+        return pickle.load(f)
+
+
+def save_following_list(following):
+    with open('following.txt', 'wb') as f:
+        pickle.dump(following, f)
+
+
+def get_following_list():
+    with open('following.txt', 'rb') as f:
+        return pickle.load(f)
+
+
+def get_last_session_cookies():
+    with open('session', 'rb') as f:
+        return pickle.load(f)
+
+
+def clean_cookies():
+    with open('session', 'rb') as f:
+        pickle.dump('', f)
+
 
 def get_total_followers(api, user_id):
     """
